195ff8b65768feb030d35745ceef24d5fb2e0a6f
gilgonzalez <fragilgon@gmail.com>
2023-10-09 19:19:21 +0200

    changes

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,14 +1,10 @@
-import { Button } from "@/components/ui/button"
-import { useBearStore } from "@/store/example"
+import { Calendar } from "@/components/ui/calendar"
 
 const Dashboard = () => {
   
-  const {bears, increase, decrease} = useBearStore(state => state)
   return (
     <div className="">
-      <h1>Dashboard { bears }</h1>
-      <Button variant={"default"} className={'bg-green-600 hover:bg-slate-400'} onClick={()=> increase(2)}>Increase Bear </Button>
-      <Button variant={"default"} className={'bg-green-600 hover:bg-slate-400'} onClick={()=> decrease(10)}>Decrease Bear </Button>
+      <Calendar/>
     </div>
   )
 }
